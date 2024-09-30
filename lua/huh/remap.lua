95fdc78a627fcccd85b4ef45ced52b5bcfe266b6
M7MDNoureddine <mohamadnoureddine@graduate.utm.my>
2024-09-30 12:24:38 +0300

    fix type in remap

--- lua/huh/remap.lua
+++ lua/huh/remap.lua
@@ -28,6 +28,6 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({seach = vim.fn.input("Grep > ")})
+	builtin.grep_string({search = vim.fn.input("Grep > ")})
 end)
 
